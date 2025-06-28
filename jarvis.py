1319c76558bf897b828c4b9acebda0fcbd07f478
pk00749 <pk00749@163.com>
2025-06-29 00:37:21 +0800

    initial

--- jarvis.py
+++ jarvis.py
@@ -1,6 +1,7 @@
 import gradio as gr
 from influence.influence import Influence
 from listen.listen import Listen
+from speak.speak import Speak
 
 def listener(audio):
     try:
@@ -10,6 +11,11 @@ def listener(audio):
         return Influence.voice_to_text(filename) #"./tests/yue.mp3"
     except Exception as e:
         return f"Fail to record voice: {e}"
+
+def speaker(filename):
+    Speak.text_to_voice()
+    print(filename)
+    return "./tests/record_output.wav"
 
 def influencer(prompt):
     return Influence.llm(prompt)
@@ -21,15 +27,21 @@ def brain(audio):
 
 def ui_launch():
     with gr.Blocks() as ui:
-        output_me = gr.Textbox(label="You")
-        output_jarvis = gr.Textbox(label="Jarvis")
-        gr.Interface(
-            fn=brain,
-            inputs=gr.Audio(sources=["microphone"]),
-            outputs=[output_me, output_jarvis],
-            title="Jarvis👾",
-            description=""
-        )
+        with gr.Row():
+            with gr.Column():
+                output_me = gr.Textbox(label="You")
+                output_jarvis_text = gr.Textbox(label="Jarvis")
+                gr.Interface(
+                    fn=brain,
+                    inputs=gr.Audio(sources=["microphone"]),
+                    outputs=[output_me, output_jarvis_text],
+                    title="Jarvis👾",
+                    description=""
+                )
+            with gr.Column():
+                output_jarvis_text.change(fn=speaker,
+                                          inputs=output_jarvis_text,
+                                          outputs=gr.Audio(sources=["microphone"], autoplay=True))
     ui.launch()
 
 
--- speak/speak.py
+++ speak/speak.py
@@ -2,3 +2,7 @@
 class Speak:
     def __init__(self):
         pass
+
+    @staticmethod
+    def text_to_voice():
+        pass
